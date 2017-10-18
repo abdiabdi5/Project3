ae6be570024ee3cee8e614f14ed2639c5ed550ff
Abdi Abdi <mrabdi100@gmail.com>
2017-10-18 15:12:58 -0500

    update 3

--- project3.py
+++ project3.py
@@ -1,9 +1,4 @@
 f = open("http_access_log","r+")
+
 for line in f:
-   print line.split(' - - ')[0]
-
-for datetime in f:
-   print datetime.strptime(' ] ')
-
-
-
+   print line.split('.*\[(.*)].*')
